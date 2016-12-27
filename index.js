b2b5dc2a22f9c1099b1db626083997651eb41f5b
Sandip Samantaray <xenosis@sandips.xyz>
2016-12-27 10:25:33 +0000

    Define default route, add static index.html

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require("express")
+var path = require("path")
+
 var app = express()
 
 app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'))
+})
+
+app.get('/:time', function(req, res) {
     res.set('Content-Type', 'text/html')
-    res.end("Hello World\n")
+    res.end(req.params.time)
 })
 
 app.listen(8080, function() {
