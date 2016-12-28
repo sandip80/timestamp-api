254afb564ccca90b107a217e22fe1e30c9647123
Sandip Samantaray <xenosis@sandips.xyz>
2016-12-28 01:21:57 +0000

    Implement timestamp service

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 var express = require("express")
 var path = require("path")
+var datetimejs = require("datetimejs")
+var timeJSONString = function(unix, natural) {
+    return JSON.stringify({"unix": unix, "natural": natural})
+}
 
 var app = express()
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/views/index.html'))
+    res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.get('/:time', function(req, res) {
-    res.set('Content-Type', 'text/html')
-    res.end(req.params.time)
+    res.set('Content-Type', 'application/json')
+    var parsedData = req.params.time
+    if (parsedData.search(/^\d+$/) == -1) {
+        parsedData = Date.parse(req.params.time)
+    } else {
+        parsedData = parseInt(parsedData, 10)
+    }
+    if (isNaN(parsedData)) {
+        res.end(timeJSONString(null, null))
+    } else {
+        var time = new Date(parsedData)
+        res.end(timeJSONString(time.getTime(), datetimejs.strftime(time, '%B %D, %Y')))
+    }
 })
 
 app.listen(8080, function() {
